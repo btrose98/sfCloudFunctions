de1ba2c1278507cbaf73edd4546d56f336ca6001
dkt201474 <dkt201474@gmail.com>
2021-02-21 06:35:50 -0500

    #server can now send notifications to mobile app

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,44 @@ admin.initializeApp();
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
 exports.alarm = functions.https.onRequest(async (req, res) => {
+  const { deviceId } = req.query;
+  if (!deviceId) return res.status(400).send('Error: Bad request');
+
+  // Get device data from firestore
+  const deviceRef = admin.firestore().collection('devices').doc(deviceId);
+  const device = await deviceRef.get();
+  if (!device.exists) return res.status(404).send('Error: Device not found');
+
+  const deviceData = device.data();
+  const deviceSubscribersIds = deviceData.subscribersIds;
+
+  // Get data for each subscriber id
+  let deviceSubscribers = await Promise.all(
+    deviceSubscribersIds.map(async (id) => {
+      const ref = await admin.firestore().collection('users').doc(id).get();
+      if (ref.exists) return ref.data();
+    })
+  );
+
+  // Remove every subscriber with no data
+  deviceSubscribers = deviceSubscribers.filter((d) => d !== undefined);
+
+  // Send notification to each subscriber
+  deviceSubscribers.forEach(async (user) => {
+    admin
+      .messaging()
+      .send({
+        topic: user.email.replace('@', '_'),
+        notification: { body: `Alarm went off at ${deviceData.address}`, title: `Hurry Up ${user.name}` },
+      })
+      .then((r) => {
+        console.log(r);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+
   // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  console.log(original);
-  // Send back a message that we've successfully written the message
-  res.json({ result: `Message with ID: ${original} added.` });
+  res.status(200).send('Success: Notification sent');
 });
